c84cd58a14295706d070a1f952bd0ceac784e015
ASIMWELA <augastinesimwela@gamil.com>
2021-01-29 16:56:34 +0200

    home component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'ant-design-vue/dist/antd.css'
 Vue.use(BootstrapVue)
 
 

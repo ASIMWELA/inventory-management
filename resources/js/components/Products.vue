cec11cf528b030d3a458bd7c89eba142ddb88567
ASIMWELA <augastinesimwela@gamil.com>
2021-02-24 15:14:06 +0200

    adding product

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <div>
+            <span>
                 <a-icon type="plus-circle" @click="showModal"/>
+
+                 <a-icon type="plus-circle" @click="showProductModal"/>
+            </span>
+
 
             <a-modal v-model="visible" title="Add Product Category" on-ok="submitCategory">
                 <template slot="footer">
@@ -20,6 +25,52 @@
                         <a-textarea v-model="category.description" />
                     </a-form-model-item>
                 </a-form-model></p>
+
+            </a-modal>
+
+            <a-modal v-model="productModalVisible" title="Add Product Category" on-ok="submitCategory">
+                <template slot="footer">
+                    <a-button key="back" @click="handleCancel">
+                        Return
+                    </a-button>
+                    <a-button key="submit" type="primary" :loading="loading" @click="submitCategory">
+                        Submit
+                    </a-button>
+                </template>
+                <p>
+                    <a-form-model :label-col="labelCol" :wrapper-col="wrapperCol">
+                        <a-form-model-item label="Name">
+                            <a-input  />
+                        </a-form-model-item>
+                        <a-form-model-item label="Category">
+                            <a-select placeholder="--please select--" >
+                                    <a-select-option  v-for="productCategory in productCategories" :value="productCategory" :key="productCategory.id">
+                                        {{productCategory.name}}
+                                     </a-select-option>
+                            </a-select>
+                        </a-form-model-item>
+                        <a-form-model-item label="Quantity">
+                           <a-input/>
+                        </a-form-model-item>
+                        <a-form-model-item label="Threshold">
+                           <a-input/>
+                        </a-form-model-item>
+                        <a-form-model-item label="Available">
+                            <a-select placeholder="--please select--">
+                                <a-select-option value="True">
+                                    true
+                                </a-select-option>
+                                <a-select-option value="False">
+                                    false
+                                </a-select-option>
+                            </a-select>
+                        </a-form-model-item>
+
+                        <a-form-model-item label="Price/unit">
+                            <a-input/>
+                        </a-form-model-item>
+                    </a-form-model>
+                </p>
 
             </a-modal>
         </div>
@@ -79,18 +130,28 @@ export default {
             name:'',
             description:''
         }
+        let productCategories = []
+
         return {
             data,
             columns,
             loading: false,
             visible: false,
-            category
+            productModalVisible:false,
+            category,
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+            productCategories
         };
     },
 
     methods: {
         showModal() {
             this.visible = true;
+        },
+
+        showProductModal() {
+            this.productModalVisible = true;
         },
         async submitCategory(e) {
             e.preventDefault();
@@ -126,5 +187,13 @@ export default {
 
         },
     },
+
+    async mounted() {
+       const categories = await Axios.get(BASE_API_URL+'/product-categories')
+       this.productCategories = categories.data.categories
+
+        console.log(this.productCategories)
+
+    }
 };
 </script>
